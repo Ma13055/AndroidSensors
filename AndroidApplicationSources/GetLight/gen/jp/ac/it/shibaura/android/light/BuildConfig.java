1c57416ba6f459e4f540299aeccf759436699976
Masaru Tatekawa <ma13055@shibaura-it.ac.jp>
2014-11-01 21:32:51 +0900

    remove
    一度全ファイル削除

--- AndroidApplicationSources/GetLight/gen/jp/ac/it/shibaura/android/light/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package jp.ac.it.shibaura.android.light;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- AndroidApplicationSources/GetOrientation/gen/jp/ac/it/shibaura/android/orientaion/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package jp.ac.it.shibaura.android.orientaion;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
